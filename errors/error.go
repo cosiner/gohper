2e955db0702c5cda165865c1df442cb75bd7c792
aihui zhu <mr.zhuah@gmail.com>
2015-01-13 19:32:32 +0800

    merge package errors' files to one

--- errors/error.go
+++ errors/error.go
@@ -1,7 +1,24 @@
 package errors
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+//==============================================================================
+//                           Assert
+//==============================================================================
+// Assert val is true, else panic error
+func Assert(val bool, err error) {
+	if !val {
+		panic(err)
+	}
+}
+
+//==============================================================================
+//                         Error Format
+//==============================================================================
 // ERR is a nil error for those condition only need an error
 var ERR = Err("")
 
@@ -33,6 +50,9 @@ func Errorf(format string, v ...interface{}) error {
 	return Err(fmt.Sprintf(format, v...))
 }
 
+//==============================================================================
+//                         Error Event
+//==============================================================================
 // OnErrExit exit process and print message on error
 func OnErrExit(err error) {
 	if err != nil {
@@ -81,4 +101,139 @@ func OnErrDoChain(err error, fns ...func(err error) error) error {
 		err = fns[i](err)
 	}
 	return err
+}
+
+//==============================================================================
+//                         Error Print
+//==============================================================================
+// ErrPrint output error message to stderr
+func PrintErr(err error) {
+	fmt.Fprint(os.Stderr, err)
+}
+
+// ErrPrintln output error message to stderr
+func PrintErrln(err error) {
+	fmt.Fprintln(os.Stderr, err)
+}
+
+// ErrFprint output error message to stderr
+func FprintErr(w io.Writer, err error) {
+	fmt.Fprint(w, err)
+}
+
+// ErrFprintln output error message to stderr
+func FprintErrln(w io.Writer, err error) {
+	fmt.Fprintln(w, err)
+}
+
+// ErrorPrint output error message to stderr
+func PrintError(v ...interface{}) {
+	fmt.Fprint(os.Stderr, v...)
+}
+
+// ErrorPrintln output error message to stderr
+func PrintErrorln(v ...interface{}) {
+	fmt.Fprintln(os.Stderr, v...)
+}
+
+// ErrorPrintf format and output error message to stderr
+func PrintErrorf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, v...)
+}
+
+// ErrorFprint output error message to stderr
+func FprintError(w io.Writer, v ...interface{}) {
+	fmt.Fprint(w, v...)
+}
+
+// ErrorFprintln output error message to stderr
+func FprintErrorln(w io.Writer, v ...interface{}) {
+	fmt.Fprintln(w, v...)
+}
+
+// ErrorFprintf output error message to stderr
+func FprintErrorf(w io.Writer, format string, v ...interface{}) {
+	fmt.Fprintf(w, format, v...)
+}
+
+//==============================================================================
+//                         Error Exit
+//==============================================================================
+
+const (
+	// ERREXIT_CODE is the default exit code on error
+	ERREXIT_CODE = 1
+	// NOREXIT_CODE is the default exit code on normal
+	NOREXIT_CODE = 0
+)
+
+// ExitWith print message, then normal exit
+func ExitWith(str string) {
+	fmt.Println(str)
+	os.Exit(NOREXIT_CODE)
+}
+
+// FexitWith print message, then normal exit
+func FexitWith(w io.Writer, str string) {
+	fmt.Fprintln(w, str)
+	os.Exit(NOREXIT_CODE)
+}
+
+// ExitOnErrPrint exit process, if error, print it
+func ExitOnErrPrint(err error) {
+	OnErrDo(err, PrintErr)
+	os.Exit(ERREXIT_CODE)
+}
+
+// ExitErr print error message then exit process
+func ExitErr(err error) {
+	FexitError(os.Stderr, err)
+}
+
+// ExitErrln print error message then exit process
+func ExitErrln(err error) {
+	FexitErrorln(os.Stderr, err)
+}
+
+// FexitErr print error message to writer then exit process
+func FexitErr(w io.Writer, err error) {
+	FexitError(w, err)
+}
+
+// FexitErrln print error message to writer then exit process
+func FexitErrln(w io.Writer, err error) {
+	FexitErrorln(w, err)
+}
+
+// ExitError print error message to stderr then exit process
+func ExitError(v ...interface{}) {
+	FexitError(os.Stderr, v...)
+}
+
+// ExitErrorln print error message to stderr then exit process
+func ExitErrorln(v ...interface{}) {
+	FexitErrorln(os.Stderr, v...)
+}
+
+// ExitErrorf  format and output error message to stderr, then exit process
+func ExitErrorf(format string, v ...interface{}) {
+	FexitErrorf(os.Stderr, format, v...)
+}
+
+// FexitError print error message to writer then exit process
+func FexitError(w io.Writer, v ...interface{}) {
+	FprintError(w, v...)
+	os.Exit(ERREXIT_CODE)
+}
+
+// FexitErrorln print error message to writer then exit process
+func FexitErrorln(w io.Writer, v ...interface{}) {
+	FprintErrorln(w, v...)
+	os.Exit(ERREXIT_CODE)
+}
+
+// FexitErrorf  format and output error message to writer, then exit process
+func FexitErrorf(w io.Writer, format string, v ...interface{}) {
+	FprintErrorf(w, format, v...)
+	os.Exit(ERREXIT_CODE)
 }
