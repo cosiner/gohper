13fe8cab465225595fdde84b488e17c6ce2fbe2c
aihui zhu <mr.zhuah@gmail.com>
2015-05-08 12:48:33 +0800

    errors:remove F*, os2: replace RunCmd with StdRunCmd

--- errors/error.go
+++ errors/error.go
@@ -2,7 +2,6 @@ package errors
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -12,119 +11,138 @@ func (e Err) Error() string {
 	return string(e)
 }
 
-// Assert assert val is true, else panic error
 func Assert(val bool, err interface{}) {
 	if !val {
 		panic(err)
 	}
 }
 
-// New make an error with given params
 func New(v ...interface{}) error {
 	return Err(fmt.Sprint(v...))
 }
 
-// Newln make an error with given params and append an newline character
 func Newln(v ...interface{}) error {
 	return Err(fmt.Sprintln(v...))
 }
 
-// Newf make an error with given format and  params
 func Newf(format string, v ...interface{}) error {
 	return Err(fmt.Sprintf(format, v...))
 }
 
-// Panic on error
 func Panic(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-// Print error message to stderr when err is not nil
+func Panicln(err error) {
+	if err != nil {
+		panic(fmt.Sprintln(err))
+	}
+}
+
+func Panicf(format string, err error) {
+	if err != nil {
+		panic(fmt.Sprintf(format, err))
+	}
+}
+
 func Print(err error) {
-	Fprint(os.Stderr, err)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
 }
 
-// Println error message to stderr when err is not nil
 func Println(err error) {
-	Fprintln(os.Stderr, err)
-}
-
-// Fprint error message to given writer when err is not nil
-func Fprint(w io.Writer, err error) {
 	if err != nil {
-		fmt.Fprint(w, err)
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
-// Fprintln error message to given writer when err is not nil
-func Fprintln(w io.Writer, err error) {
+func Printf(format string, err error) {
 	if err != nil {
-		fmt.Fprint(w, err)
+		fmt.Fprintf(os.Stderr, format, err)
 	}
 }
 
-// Exit if error is not nil, print error message to Stdout and exit code is 0
 func Exit(err error) {
-	Fexit(os.Stdout, err)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(0)
+	}
 }
 
-// Exitln if error is not nil, print error message to Stdout and exit code is 0
 func Exitln(err error) {
-	Fexitln(os.Stdout, err)
-}
-
-// Fexit if error is not nil, print error message to Writer and exit code is 0
-func Fexit(w io.Writer, err error) {
 	if err != nil {
-		fmt.Fprint(w, err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(0)
 	}
 }
 
-// Fexitln if error is not nil, print error message to Writer and exit code is 0
-func Fexitln(w io.Writer, err error) {
+func Exitf(format string, err error) {
 	if err != nil {
-		fmt.Fprintln(w, err)
+		fmt.Fprintf(os.Stderr, format, err)
 		os.Exit(0)
 	}
 }
 
-// Fatal print message, then Fatal with -1 as error code when err is not nil
 func Fatal(err error) {
-	Ffatal(os.Stderr, err)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(-1)
+	}
 }
 
-// Fatalln print message, then Fatal with  -1 as error code when err is not nil
 func Fatalln(err error) {
-	Ffatalln(os.Stderr, err)
-}
-
-// Ffatal print message, then exit with  -1 as error code when err is not nil
-func Ffatal(w io.Writer, err error) {
 	if err != nil {
-		fmt.Fprint(w, err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 }
 
-// Ffatalln print message, then exit with  -1 as error code when err is not nil
-func Ffatalln(w io.Writer, err error) {
+func Fatalf(format string, err error) {
 	if err != nil {
-		fmt.Fprintln(w, err)
+		fmt.Fprintf(os.Stderr, format, err)
 		os.Exit(-1)
 	}
 }
 
-// Do call param function when err is not null when err is not nil
+func ExitAny(err error) {
+	fmt.Fprint(os.Stderr, err)
+	os.Exit(0)
+}
+
+func ExitAnyf(format string, err error) {
+	fmt.Fprintln(os.Stderr, format, err)
+	os.Exit(0)
+}
+
+func ExitAnyln(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(0)
+}
+
+func FatalAny(err error) {
+	fmt.Fprint(os.Stderr, err)
+	os.Exit(-1)
+}
+
+func FatalAnyf(format string, err error) {
+	fmt.Fprint(os.Stderr, format, err)
+	os.Exit(-1)
+}
+
+func FatalAnyln(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(-1)
+}
+
 func Do(err error, fn func(err error)) {
 	if err != nil {
 		fn(err)
 	}
 }
 
-// CondDo call function if val is true
 func CondDo(val bool, err error, fn func(error)) {
 	if val {
 		fn(err)
--- os2/cmd.go
+++ os2/cmd.go
@@ -4,32 +4,45 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
-// RunCmd run commond, if reader or writer is nil, use os.Stdin/out/err,
-// first elelemts of args should be the command to execute
-func RunCmd(in io.Reader, out, err io.Writer, args ...string) error {
+// StdRunCmd run commond, if reader or writer is nil, use os.Stdin/out/err,
+func StdRunCmd(in io.Reader, out, err io.Writer, args ...string) error {
 	if in == nil {
 		in = os.Stdin
 	}
+
 	if out == nil {
 		out = os.Stdout
 	}
 	if err == nil {
 		err = os.Stderr
 	}
+	return RunCmd(in, out, err, args...)
+}
 
-	var c *exec.Cmd
-	if l := len(args); l == 0 {
+// RunCmd is a quick way to exec.Command().Run,
+// first elelemts of args should be the command to execute
+func RunCmd(in io.Reader, out, err io.Writer, args ...string) error {
+	if len(args) == 0 {
 		panic("no command to run")
-	} else if l == 1 {
-		c = exec.Command(args[0])
-	} else {
-		c = exec.Command(args[0], args[1:]...)
 	}
-	c.Stdin = in
-	c.Stdout = out
-	c.Stderr = err
 
-	return c.Run()
+	name := args[0]
+	if filepath.Base(name) == name {
+		if lp, err := exec.LookPath(name); err != nil {
+			return err
+		} else {
+			name = lp
+		}
+	}
+
+	return (&exec.Cmd{
+		Path:   name,
+		Args:   args,
+		Stdin:  in,
+		Stdout: out,
+		Stderr: err,
+	}).Run()
 }
