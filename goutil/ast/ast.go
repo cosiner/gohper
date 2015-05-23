ce860c6f5421f2ed9fa19b43aaf78eed4c99243f
aihui zhu <mr.zhuah@gmail.com>
2015-05-23 21:57:27 +0800

    goutil/ast: add Doc, Var, bugfix: empty struct

--- goutil/ast/ast.go
+++ goutil/ast/ast.go
@@ -29,19 +29,31 @@ type Attrs struct {
 	// Common
 	TypeName string
 
+	// Document
+	TypeDoc []string
+
 	// Struct
 	S struct {
+		Doc         []string
 		Field, Type string // if type is empty, means anonymous field
 		Tag         reflect.StructTag
 	}
 
 	// Const
 	C struct {
+		Doc         []string
 		Name, Value string
+	}
+
+	// Var
+	V struct {
+		Doc        []string
+		Name, Type string
 	}
 
 	// Interface
 	I struct {
+		Doc    []string
 		Method string
 	}
 
@@ -54,9 +66,12 @@ type Attrs struct {
 
 type Parser struct {
 	Const     func(*Attrs) error
+	Var       func(*Attrs) error
 	Interface func(*Attrs) error
 	Struct    func(*Attrs) error
 	Func      func(*Attrs) error
+
+	ParseDoc bool
 }
 
 func (p Parser) ParseFile(fname string) error {
@@ -84,17 +99,18 @@ func (p Parser) Parse(file *ast.File) (err error) {
 		switch decl := decl.(type) {
 		case *ast.GenDecl:
 			if decl.Tok == token.TYPE {
-				if p.Struct == nil || p.Interface == nil {
+				if p.Struct == nil && p.Interface == nil {
 					continue
 				}
-
-				for _, spec := range decl.Specs {
-					if err = p.parseType(spec.(*ast.TypeSpec), attrs); err != nil {
-						return
-					}
+				if err = p.parseType(decl, attrs); err != nil {
+					return
 				}
 			} else if decl.Tok == token.CONST && p.Const != nil {
 				if err = p.parseConsts(decl, attrs); err != nil {
+					return
+				}
+			} else if decl.Tok == token.VAR && p.Var != nil {
+				if err = p.parseVars(decl, attrs); err != nil {
 					return
 				}
 			}
@@ -111,23 +127,38 @@ func (p Parser) Parse(file *ast.File) (err error) {
 	return
 }
 
-func (p Parser) parseType(spec *ast.TypeSpec, attrs *Attrs) error {
-	attrs.TypeName = ""
-	switch typ := spec.Type.(type) {
-	case *ast.StructType:
-		if p.Struct != nil {
-			attrs.TypeName = spec.Name.Name
-			return p.parseStruct(typ, attrs)
+func (p Parser) parseType(decl *ast.GenDecl, attrs *Attrs) error {
+	attrs.TypeDoc = nil
+	l := len(decl.Specs)
+	if l == 1 {
+		attrs.TypeDoc = p.parseDoc(decl.Doc)
+	}
+
+	var err error
+	for i := 0; i < l && err == nil; i++ {
+		attrs.TypeName = ""
+		spec := decl.Specs[i].(*ast.TypeSpec)
+
+		if doc := p.parseDoc(spec.Doc); doc != nil {
+			attrs.TypeDoc = doc
 		}
 
-	case *ast.InterfaceType:
-		if p.Interface != nil {
-			attrs.TypeName = spec.Name.Name
-			return p.parseInterface(typ, attrs)
+		switch typ := spec.Type.(type) {
+		case *ast.StructType:
+			if p.Struct != nil {
+				attrs.TypeName = spec.Name.Name
+				err = p.parseStruct(typ, attrs)
+			}
+
+		case *ast.InterfaceType:
+			if p.Interface != nil {
+				attrs.TypeName = spec.Name.Name
+				err = p.parseInterface(typ, attrs)
+			}
 		}
 	}
 
-	return nil
+	return err
 }
 
 func (p Parser) parseStruct(spec *ast.StructType, attrs *Attrs) (err error) {
@@ -135,6 +166,8 @@ func (p Parser) parseStruct(spec *ast.StructType, attrs *Attrs) (err error) {
 
 	for _, f := range spec.Fields.List {
 		attrs.S.Type = ""
+		attrs.S.Doc = p.parseDoc(f.Doc)
+
 		if f.Type != nil {
 			attrs.S.Type = fmt.Sprint(f.Type)
 		}
@@ -154,6 +187,10 @@ func (p Parser) parseStruct(spec *ast.StructType, attrs *Attrs) (err error) {
 		}
 	}
 
+	if len(spec.Fields.List) == 0 {
+		p.Struct(attrs)
+	}
+
 	return
 }
 
@@ -161,12 +198,18 @@ func (p Parser) parseInterface(spec *ast.InterfaceType, attrs *Attrs) (err error
 	defer nonTypeEnd(&err)
 
 	for _, m := range spec.Methods.List {
+		attrs.I.Doc = p.parseDoc(m.Doc)
+
 		for _, n := range m.Names {
 			attrs.I.Method = n.Name
 			if err = p.Interface(attrs); err != nil {
 				return
 			}
 		}
+	}
+
+	if len(spec.Methods.List) == 0 {
+		p.Interface(attrs)
 	}
 
 	return
@@ -176,7 +219,10 @@ func (p Parser) parseConsts(decl *ast.GenDecl, attrs *Attrs) (err error) {
 	defer nonTypeEnd(&err)
 
 	attrs.TypeName = ""
-	for i, l := 0, len(decl.Specs); i < l; i++ {
+	attrs.TypeDoc = p.parseDoc(decl.Doc)
+
+	l := len(decl.Specs)
+	for i, l := 0, l; i < l; i++ {
 		spec := decl.Specs[i].(*ast.ValueSpec)
 
 		if spec.Type != nil {
@@ -189,11 +235,44 @@ func (p Parser) parseConsts(decl *ast.GenDecl, attrs *Attrs) (err error) {
 		for i, name := range spec.Names {
 			attrs.C.Name = name.Name
 			attrs.C.Value = ""
+			attrs.C.Doc = p.parseDoc(spec.Doc)
+
 			if i < vlen {
 				attrs.C.Value = fmt.Sprint(spec.Values[i])
 			}
 
 			if err = p.Const(attrs); err != nil {
+				return
+			}
+		}
+	}
+	if l == 0 && attrs.TypeName != "" {
+		p.Const(attrs)
+	}
+
+	return
+}
+
+func (p Parser) parseVars(decl *ast.GenDecl, attrs *Attrs) (err error) {
+	defer nonTypeEnd(&err)
+
+	attrs.TypeName = ""
+	attrs.TypeDoc = p.parseDoc(decl.Doc)
+
+	l := len(decl.Specs)
+	for i, l := 0, l; i < l; i++ {
+		spec := decl.Specs[i].(*ast.ValueSpec)
+		if spec.Type != nil {
+			attrs.V.Type = fmt.Sprint(spec.Type)
+		} else {
+			attrs.V.Type = "" // iota is break out
+		}
+
+		for _, name := range spec.Names {
+			attrs.V.Name = name.Name
+			attrs.V.Doc = p.parseDoc(spec.Doc)
+
+			if err = p.Var(attrs); err != nil {
 				return
 			}
 		}
@@ -206,6 +285,7 @@ func (p Parser) parseFunc(decl *ast.FuncDecl, attrs *Attrs) (err error) {
 	attrs.TypeName = ""
 	attrs.F.PtrRecv = false
 	attrs.F.Name = decl.Name.Name
+	attrs.TypeDoc = p.parseDoc(decl.Doc)
 
 	if decl.Recv != nil {
 		switch recv := decl.Recv.List[0].Type.(type) {
@@ -221,6 +301,19 @@ func (p Parser) parseFunc(decl *ast.FuncDecl, attrs *Attrs) (err error) {
 	nonTypeEnd(&err)
 
 	return
+}
+
+func (p Parser) parseDoc(cg *ast.CommentGroup) []string {
+	if !p.ParseDoc || cg == nil {
+		return nil
+	}
+
+	docs := make([]string, len(cg.List))
+	for i, c := range cg.List {
+		docs[i] = c.Text
+	}
+
+	return docs
 }
 
 func nonTypeEnd(err *error) {
