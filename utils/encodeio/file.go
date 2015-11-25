bae0903c957b939c06a166e26fc349d737b258fd
aihui zhu <cosiner@outlook.com>
2015-11-26 07:21:26 +0800

    utils: add ReadJSONWithComment

--- utils/encodeio/file.go
+++ utils/encodeio/file.go
@@ -4,8 +4,16 @@ package encodeio
 import (
 	"os"
 
+	"bufio"
+	"bytes"
+	"io"
+
 	"github.com/cosiner/gohper/encoding"
 	"github.com/cosiner/gohper/os2/file"
+)
+
+var (
+	commentPrefix = []byte("//")
 )
 
 func Read(fname string, v interface{}, codec encoding.Codec) error {
@@ -16,6 +24,30 @@ func Read(fname string, v interface{}, codec encoding.Codec) error {
 
 func ReadJSON(fname string, v interface{}) error {
 	return Read(fname, v, encoding.JSON{})
+}
+
+func ReadJSONWithComment(fname string, v interface{}) error {
+	return file.Read(fname, func(fd *os.File) error {
+		buf := bytes.NewBuffer(make([]byte, 0, 1024))
+		br := bufio.NewReader(fd)
+
+		for {
+			line, _, err := br.ReadLine()
+			if err == io.EOF {
+				if len(line) == 0 {
+					break
+				}
+			} else if err != nil {
+				return err
+			}
+
+			if !bytes.HasPrefix(bytes.TrimSpace(line), commentPrefix) {
+				buf.Write(line)
+			}
+		}
+
+		return encoding.JSON{}.Unmarshal(buf.Bytes(), v)
+	})
 }
 
 func Write(fname string, v interface{}, codec encoding.Codec) error {
