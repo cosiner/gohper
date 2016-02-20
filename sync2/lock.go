e0c5fc9b96907d0752fa207bdf452015f3d0d155
aihui zhu <mr.zhuah@gmail.com>
2016-02-20 16:18:50 +0800

    sync2: replace MRWMutex with AutorefMutex

--- sync2/lock.go
+++ sync2/lock.go
@@ -1,76 +1,10 @@
 package sync2
 
 import (
+	"fmt"
 	"sync"
 	"sync/atomic"
-	"unsafe"
 )
-
-const APPEND = "01235"
-
-type MRWMutex struct {
-	lock  sync.RWMutex
-	locks map[string]*sync.RWMutex
-}
-
-func NewMRWMutex() *MRWMutex {
-	return &MRWMutex{
-		locks: make(map[string]*sync.RWMutex),
-	}
-}
-
-func (m *MRWMutex) NewId(id string) string {
-	for {
-		if _, b := m.locker(id); b {
-			return id
-		}
-		id = id + APPEND[uintptr(unsafe.Pointer(&id))&4:]
-	}
-}
-
-func (m *MRWMutex) locker(lockId string) (l *sync.RWMutex, b bool) {
-	m.lock.RLock()
-	if l = m.locks[lockId]; l == nil {
-		m.lock.RUnlock()
-
-		m.lock.Lock()
-		if l = m.locks[lockId]; l == nil {
-			l = &sync.RWMutex{}
-			m.locks[lockId] = l
-			b = true
-		}
-		m.lock.Unlock()
-	} else {
-		m.lock.RUnlock()
-	}
-
-	return
-}
-
-func (m *MRWMutex) locker2(lockId string) *sync.RWMutex {
-	l, _ := m.locker(lockId)
-	return l
-}
-
-func (m *MRWMutex) RLocker(lockId string) sync.Locker {
-	return m.locker2(lockId).RLocker()
-}
-
-func (m *MRWMutex) Lock(lockId string) {
-	m.locker2(lockId).Lock()
-}
-
-func (m *MRWMutex) Unlock(lockId string) {
-	m.locker2(lockId).Unlock()
-}
-
-func (m *MRWMutex) RLock(lockId string) {
-	m.locker2(lockId).RLock()
-}
-
-func (m *MRWMutex) RUnlock(lockId string) {
-	m.locker2(lockId).RUnlock()
-}
 
 // Spinlock should not be used on single cpu(core) machines.
 // if one goroute take out the lock, the programm will be died if another goroutine
@@ -85,5 +19,137 @@ func (s *Spinlock) Lock() {
 func (s *Spinlock) Unlock() {
 	if !atomic.CompareAndSwapInt32((*int32)(s), 1, 0) {
 		panic("unlock unlocked spinlock")
+	}
+}
+
+type rwLocker interface {
+	sync.Locker
+	RLock()
+	RUnlock()
+	Ref() int
+	SetRef(int)
+}
+
+type mutexRef int
+
+func (m *mutexRef) Ref() int {
+	return int(*m)
+}
+
+func (m *mutexRef) SetRef(ref int) {
+	*m = mutexRef(ref)
+}
+
+type refRWMutex struct {
+	mutexRef
+	sync.RWMutex
+}
+
+type refMutex struct {
+	mutexRef
+	sync.Mutex
+}
+
+func (r *refMutex) RLock() {
+	r.Mutex.Lock()
+}
+
+func (r *refMutex) RUnlock() {
+	r.Mutex.Unlock()
+}
+
+type AutorefMutex struct {
+	mu   sync.Mutex
+	pool sync.Pool
+
+	mus map[string]rwLocker
+}
+
+func NewAutorefMutex(rw bool) *AutorefMutex {
+	var new func() interface{}
+	if rw {
+		new = func() interface{} {
+			return &refRWMutex{}
+		}
+	} else {
+		new = func() interface{} {
+			return &refMutex{}
+		}
+	}
+	return &AutorefMutex{
+		pool: sync.Pool{
+			New: new,
+		},
+		mus: make(map[string]rwLocker),
+	}
+}
+
+func (m *AutorefMutex) newLocker(key string) rwLocker {
+	mu := m.pool.Get().(rwLocker)
+	mu.SetRef(0)
+	m.mus[key] = mu
+	return mu
+}
+
+func (m *AutorefMutex) freeLocker(mu rwLocker) {
+	m.pool.Put(mu)
+}
+
+func (m *AutorefMutex) locker(key string, unlock bool) (recyle bool, mu rwLocker) {
+	m.mu.Lock()
+	mu, has := m.mus[key]
+	if unlock {
+		if !has {
+			m.mu.Unlock()
+			panic("unlock unexisted key")
+		}
+		ref := mu.Ref()
+
+		if ref < 1 {
+			m.mu.Unlock()
+			panic(fmt.Sprintf("impossible reference count: %d for key: %s", ref, key))
+		}
+
+		mu.SetRef(ref - 1)
+		recyle = ref == 1
+		if recyle {
+			delete(m.mus, key)
+		}
+	} else {
+		if !has {
+			mu = m.newLocker(key)
+		}
+		mu.SetRef(mu.Ref() + 1)
+	}
+	m.mu.Unlock()
+
+	return recyle, mu
+}
+
+func (m *AutorefMutex) Lock(key string) {
+	_, mu := m.locker(key, false)
+	mu.Lock()
+}
+
+func (m *AutorefMutex) Unlock(key string) {
+	// unlock mean works done, it's safe to create another mutex with same key
+	// even if current mutex hasn't been unlocked really.
+	recyle, mu := m.locker(key, true)
+	mu.Unlock()
+	if recyle {
+		m.freeLocker(mu)
+	}
+}
+
+func (m *AutorefMutex) RLock(key string) {
+	_, mu := m.locker(key, false)
+	mu.RLock()
+}
+
+func (m *AutorefMutex) RUnlock(key string) {
+	recyle, mu := m.locker(key, true)
+	mu.RUnlock()
+	if recyle {
+		m.freeLocker(mu)
 	}
 }
--- sync2/lock_test.go
+++ sync2/lock_test.go
@@ -1,7 +1,10 @@
 package sync2
 
 import (
+	"fmt"
+	"math/rand"
 	"runtime"
+	"sync"
 	"testing"
 	"time"
 
@@ -27,4 +30,41 @@ func TestSpinLock(t *testing.T) {
 
 	defer tt.Recover()
 	lock.Unlock()
+}
+
+func TestAutorefMutex(t *testing.T) {
+	t.Log("start testing.")
+	mu := NewAutorefMutex(false)
+
+	keys := []string{"1", "2", "3"}
+	keyNum := len(keys)
+	wg := sync.WaitGroup{}
+
+	routine := 21
+	wg.Add(routine)
+
+	for i := 0; i < routine; i++ {
+		n := i + 1
+		go func() {
+			time.Sleep(time.Duration(n%3)*time.Millisecond)
+			key := keys[rand.Intn(keyNum)]
+			mu.Lock(key)
+			t.Logf("routine %d locked %s", n, key)
+			time.Sleep(time.Duration(n%3)*time.Millisecond)
+			mu.Unlock(key)
+			t.Logf("routine %d unlocked %s", n, key)
+
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkAutorefMutex(b *testing.B) {
+	mu := NewAutorefMutex(false)
+
+	for i := 0; i < b.N; i++ {
+		mu.Lock("a")
+		mu.Unlock("a")
+	}
 }
